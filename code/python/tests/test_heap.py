a85c6448c37ff2495b26bc8d893270b0be1b2109
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-09-10 16:01:55 -0300

    add test for heap printing

--- code/python/tests/test_heap.py
+++ code/python/tests/test_heap.py
@@ -1,6 +1,17 @@
-def test_heap():
-    from python.heap import Heap
+from python.heap import Heap
 
+
+def test_heap():
     h = Heap()
     h.insert(3)
     assert h.head.value == 3
+
+
+def test_heap_str():
+    h = Heap()
+    h.insert(3)
+    h.insert(2)
+    h.insert(1)
+
+    expected_str = "3 -> 2 -> 1"
+    assert str(h) == expected_str
