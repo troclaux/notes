0f91d0490fe6eafd3a802e912c5bbe096eba6e9f
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-09-14 10:30:15 -0300

    implement stack

--- code/python/stack.py
+++ code/python/stack.py
@@ -1,19 +1,26 @@
 class Stack:
     def __init__(self):
-        self.arrows = []
+        self.stack = []
+        self.size = 0
 
-    def push(self, arrow):
-        self.arrows.append(arrow)
+    def push(self, value):
+        self.stack.append(value)
+        self.size += 1
 
     def pop(self):
-        if len(self.arrows) < 1:
-            return None
-        return self.arrows.pop()
+        if self.is_empty():
+            raise IndexError("can't pop when stack is empty")
+        removed_value = self.stack.pop()
+        self.size -= 1
+        return removed_value
 
     def peek(self):
-        if len(self.arrows) < 1:
-            return None
-        return self.arrows[-1]
+        if self.is_empty():
+            raise IndexError("can't peek when stack is empty")
+        print(self.stack[-1])
 
-    def size(self):
-        return len(self.arrows)
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    def __str__(self):
+        print(str(self.stack))
