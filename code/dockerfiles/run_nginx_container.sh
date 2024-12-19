0fcab1492009d671b5d887ceefad73c237aab735
Arthur Albuquerque <20231694+troclaux@users.noreply.github.com>
2024-12-19 09:28:17 -0300

    reposition dockerfiles

--- code/dockerfiles/run_nginx_container.sh
+++ code/dockerfiles/run_nginx_container.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-path="$(pwd)/dockerfiles"
+path="~/Documents/notes/code/dockerfiles"
 echo "Script path: $path"
 cd $path
 docker image build -t nginx-image -f ./nginx.Dockerfile .
-docker container run -p 8080:80 -v ~/Documents/cheat-sheets/dockerfiles/volume:/arthur --name my-nginx -d nginx-image
+docker container run -p 8080:80 -v ~/Documents/notes/code/dockerfiles/volume:/arthur --name my-nginx -d nginx-image
 docker exec -it my-nginx /bin/bash
 # docker container run --p 8080:80 -name my-nginx --rm nginx-image
